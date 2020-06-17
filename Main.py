e331969b00b6e3444e3014b3f6c384c3b9312850
beverneus <36573562+beverneus@users.noreply.github.com>
2020-06-17 19:15:26 +0200

    Made BitcoinPrice loop run asynchronous

--- Main.py
+++ Main.py
@@ -1,25 +1,29 @@
 # import all modules
 from config import BOT_TOKEN
 import logging
-from telegram.ext import CommandHandler, Updater
+import telegram
+from telegram.ext import CommandHandler, Updater, CallbackContext
 import time
 import requests
 DELAY = 6  # Hours
 # Token is located in config.py
 
+
 updater = Updater(token=BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
+job_queue = updater.job_queue
 
 # Logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - , %(message)s", level=logging.INFO)
+    format="%(asctime)s - %(name)s - %(levelname)s - , %(message)s",
+    level=logging.INFO)
 
 # Define Functions
 
 # Imports the bitcoin price from the coincap api
 
 
-def get_latest_bitcoin_price():
+def getBitcoinPrice():
     response = requests.get('https://api.coincap.io/v2/assets/bitcoin/')
     response_json = response.json()
     # Convert the price to a floating point number
@@ -30,23 +34,30 @@ def get_latest_bitcoin_price():
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Thank you for purchasing my bot, type '/price' to start")
+                             text="Thank you for purchasing my bot,"
+                             " type '/price' to start")
 
 # Sends price every DELAY seconds
 
 
-def loop(update, context):
-    while True:
-        price = round(get_latest_bitcoin_price(), 2)
-        timedate = time.strftime('[%H:%M/%d.%m.%Y]')
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"{timedate} : {price}")
-        time.sleep(DELAY)
+def bPrice(context: CallbackContext):
+    price = round(getBitcoinPrice(), 2)
+    timedate = time.strftime('[%H:%M/%d.%m.%Y]')
+    context.bot.send_message(chat_id=context.job.context,
+                             text=f"{timedate} : {price}")
+
+
+def repeat(update: telegram.Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text='Starting Bitcoin Price Alert')
+    repeater = job_queue.run_repeating(bPrice, interval=10, first=0,
+                                       context=update.message.chat_id)
 
 
 # Define command handelers and add them to dispatcher
 start_handeler = CommandHandler("start", start)
 dispatcher.add_handler(start_handeler)
-loop_handeler = CommandHandler("price", loop)
+loop_handeler = CommandHandler("price", repeat)
 dispatcher.add_handler(loop_handeler)
 
 # Start listening to commands
